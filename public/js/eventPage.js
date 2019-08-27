26c9f25e5edd58c7baa4ba8dde7cdf015aaf0381
David Zachary Milewicz <milewicz@seas.upenn.edu>
2019-08-26 23:08:35 -0700

    cleaned up the code some

--- public/js/eventPage.js
+++ public/js/eventPage.js
@@ -3,108 +3,98 @@
  * string will be saved on this page. Upon the next page load, if there is a string in the event page it will be passed to the next page an search for the string will commence. 
  */
 
+// dict to hold the tab icon states
 var tab_badges = {};
+
+// dict for holding the active page info
 var pages = {}
 
-// setInterval(function() {
-// 	for (tab in pages) {
-// 		chrome.tabs.sendMessage(parseInt(tab), pages[tab]);
-// 	}
-// 	console.log(pages);
-// }, 500);
-
-
+/*
+ *
+ */
 function active_conn(msg, tabId) {
-	// alert("running!");
-	if (tabId in pages) {
-		console.log("once");
-		chrome.tabs.sendMessage(tabId, msg); 
-		setTimeout(function(){ active_conn(msg, tabId); }, 500);
+    // alert("running!");
+    if (tabId in pages) {
+        console.log("once");
+        chrome.tabs.sendMessage(tabId, msg); 
+        setTimeout(function(){ active_conn(msg, tabId); }, 500);
 
-	} else {
-		console.log("err not found");
-
-		// no more calls
-	}
+    } else {
+        console.log("err not found");
+        // no more calls
+        return
+    }
 }
 
 
 chrome.runtime.onMessage.addListener(
-	function(message, sender, sendResponse) {
+    function(message, sender, sendResponse) {
 
-		debug_log("Recieved " + message.type + " at event page");
+        debug_log("Recieved " + message.type + " at event page");
 
-		if (!message.type) {
-			debug_log("ERR: No type");
-			return;
-		}
+        // This means something I don't remember what
+        if (!message.type) {
+            debug_log("ERR: No type");
+            return;
+        }
 
-		switch (message.type) {
-			// receive from google page tab the string to search for
-			case SEARCH_STRING_MSG: 
-				
+        switch (message.type) {
+            // receive serch string from Google page
+            case SEARCH_STRING_MSG: 
+                chrome.tabs.onUpdated.addListener(function sendString(tabId, info, tab) {
+                    if (tabId == sender.tab.id) { // make sure correct tab
 
-				chrome.tabs.onUpdated.addListener(function sendString(tabId, info, tab) {
-					if (tabId == sender.tab.id) { // make sure correct tab
+                        // log info
+                        console.log(info);
+                        console.log(message.searchString);
 
-						// log info
-						console.log(info);
-						console.log(message.searchString);
+                        // construct message to be relayed
+                        var relay_msg = { 
+                            type: RELAYED_SEARCH_STRING_MSG, 
+                            searchString : message.searchString, 
+                            page : message.page,  
+                        };
 
-						// construct message to be relayed
-						var relay_msg = { 
-							type: RELAYED_SEARCH_STRING_MSG, 
-							searchString : message.searchString, 
-							page : message.page,  
-						};
+                        // send to the front tab, record sent message
+                        chrome.tabs.sendMessage(sender.tab.id, relay_msg);
+                        
+                        // add 
+                        pages[sender.tab.id] = relay_msg;
+                        active_conn(relay_msg, sender.tab.id);
+                        
+                        // remove the listener when triggered
+                        chrome.tabs.onUpdated.removeListener(sendString);
+                    }
+                });
+                break;
 
-						// send to the front tab, record sent message
-						chrome.tabs.sendMessage(sender.tab.id, relay_msg);
-						pages[sender.tab.id] = relay_msg;
-						active_conn(relay_msg, sender.tab.id);
+            // received search results from the content page
+            case SEARCH_RESULTS_MSG: 
+                // remove from list of active connections
+                delete pages[sender.tab.id];
+                console.log("deleted");
 
-
-						// setTimeout(function() {
-						// 	if (pages[sender.tab.id]) {
-						// 		alert("hello!");
-						// 		chrome.tabs.sendMessage(sender.tab.id, relayed_msg); 
-						// 	}
-						// }, 2000);
-
-
-						// remove the listener when triggered
-						chrome.tabs.onUpdated.removeListener(sendString);
-					}
-				});
-				break;
-
-			// check ofr badge update condition
-			case SEARCH_RESULTS_MSG: 
-				delete pages[sender.tab.id];
-				console.log("deleted");
-				badgeUpdate(sender.tab.id, message.result);
-				break;
-		}
-	}
+                // update the badge 
+                badgeUpdate(sender.tab.id, message.result);
+                break;
+        }
+    }
 );
-		
-// chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
-// 	if (info.url) { // url has changed, clear the badge
-// 		badgeUpdate(tabId, "");
-// 	}
-// });
 
+/*
+ * Update the badge state
+ * @param id the chrome tab ID
+ * @param msg status text to display on chrome
+ */
 function badgeUpdate(id, msg) {
-	if (msg == SEARCH_SUCCESS) {
-		chrome.browserAction.setBadgeBackgroundColor({ color: GREEN, tabId:id});
-	}
+    if (msg == SEARCH_SUCCESS) {
+        chrome.browserAction.setBadgeBackgroundColor({ color: GREEN, tabId:id});
+    }
 
-	if (msg == SEARCH_FAILURE) {
-		chrome.browserAction.setBadgeBackgroundColor({ color: RED, tabId:id});
-	}
+    if (msg == SEARCH_FAILURE) {
+        chrome.browserAction.setBadgeBackgroundColor({ color: RED, tabId:id});
+    }
 
-	chrome.browserAction.setBadgeText({ text: msg , tabId : id});
-	tab_badges[id] = msg;
+    // Set text according to results
+    chrome.browserAction.setBadgeText({ text: msg , tabId : id});
 }
-
-	
--- public/js/googlePage.js
+++ public/js/googlePage.js
@@ -10,15 +10,15 @@ var onMouseDownEvent = "class=\"addedLink\"";
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 $(function() {
-	console.log("running identify...");
+    console.log("running identify...");
     identifyMessage();
 });
 
 var counter = 0
 var observer = new MutationObserver(function(mutations, observer) {
     console.log(mutations);
-	console.log("running identify " + counter);
-	
+    console.log("running identify " + counter);
+    
 
     identifyMessage();
 });
@@ -26,19 +26,19 @@ var observer = new MutationObserver(function(mutations, observer) {
 // define what element should be observed by the observer
 // and what types of mutations trigger the callback
 observer.observe($("#gsr").get(0), {
-	childList : true,
-	characterData : true
+    childList : true,
+    characterData : true
 
 });
 
 
 function identifyMessage() {
-	
-	var arraySpan = document.getElementsByClassName("g");
-	
-	$(".g").each(function(i, obj) {
-		injectHyperlink(obj);
-	});
+    
+    var arraySpan = document.getElementsByClassName("g");
+    
+    $(".g").each(function(i, obj) {
+        injectHyperlink(obj);
+    });
 };
 
 
@@ -51,61 +51,60 @@ function identifyMessage() {
  *
  */
 function injectHyperlink(message) {
-	
-	if (message === undefined) { 
-		console.log("g class element undefined");
-		return; 
-	}
-	
-	var titleElement = message;
-	
-	if (titleElement === undefined) {
-		return;
-	}
-	   
-	// get link to next page 
-	var link = titleElement.getElementsByTagName("a")[0].href;
-	   
-	// get result contents 
-	var stContent = message.getElementsByClassName("st")[0];
-	
-	if (stContent === undefined) {
-		return;
-	}
-	
-	// get result date if present
-	var fContent = stContent.getElementsByClassName("f")[0];
-	
-	// create new html string
-	var newContent = "";
-	
-	// if present add the date to the new HTML string
-	if (fContent !== undefined) {
-		newContent += fContent.outerHTML;
-		stContent.removeChild(fContent);
-	}
-	
-	// get relevant text from the webpage
-	var stText = stContent.innerHTML;
-	
-	// split by elipsis
-	var contentFragments = stText.split(/\s?\.\.+\s?/);
-	   
-	// add href links
-	for (var i = 0; i < contentFragments.length; i++) {
-		newContent += "<a " + onMouseDownEvent + " style=\"color: inherit; text-decoration: none;\" href=\"" + link + "\">" + contentFragments[i] + "</a>";
-		if (i != (contentFragments.length - 1) && contentFragments[i] != "") {
-			newContent += "...";
-		}
-	}
-	   
-	// refill element content
-	stContent.innerHTML = newContent;
+    
+    if (message === undefined) { 
+        console.log("g class element undefined");
+        return; 
+    }
+    
+    var titleElement = message;
+    
+    if (titleElement === undefined) {
+        return;
+    }
+       
+    // get link to next page 
+    var link = titleElement.getElementsByTagName("a")[0].href;
+       
+    // get result contents 
+    var stContent = message.getElementsByClassName("st")[0];
+    
+    if (stContent === undefined) {
+        return;
+    }
+    
+    // get result date if present
+    var fContent = stContent.getElementsByClassName("f")[0];
+    
+    // create new html string
+    var newContent = "";
+    
+    // if present add the date to the new HTML string
+    if (fContent !== undefined) {
+        newContent += fContent.outerHTML;
+        stContent.removeChild(fContent);
+    }
+    
+    // get relevant text from the webpage
+    var stText = stContent.innerHTML;
+    
+    // split by elipsis
+    var contentFragments = stText.split(/\s?\.\.+\s?/);
+       
+    // add href links
+    for (var i = 0; i < contentFragments.length; i++) {
+        newContent += "<a " + onMouseDownEvent + " style=\"color: inherit; text-decoration: none;\" href=\"" + link + "\">" + contentFragments[i] + "</a>";
+        if (i != (contentFragments.length - 1) && contentFragments[i] != "") {
+            newContent += "...";
+        }
+    }
+       
+    // refill element content
+    stContent.innerHTML = newContent;
 
-	$(".addedLink").off().on('mousedown', function() {
-		// debug_log("GOOGLE_PAGE: Sending messsage to event page. message:\n" + this.innerText);
-		chrome.runtime.sendMessage({ type: SEARCH_STRING_MSG, searchString : this.innerText , page : this.href });
-	});
+    $(".addedLink").off().on('mousedown', function() {
+        chrome.runtime.sendMessage({ type: SEARCH_STRING_MSG, searchString : this.innerText , page : this.href });
+    });
 };
    
    
